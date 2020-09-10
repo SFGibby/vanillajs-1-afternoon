79f2b8bcfae5dd6944fdc2e65d439c7cd672016b
SFGibby <samgibson124@gmail.com>
2020-09-10 01:16:02 -0600

    Finished this signifiantly later than initially planned

---
+++ Part2/index.js
@@ -0,0 +1,10 @@
+const idInput = document.getElementById('idInput');
+const colorInput = document.getElementById('colorInput');
+function setCard () {
+    const card = document.getElementById(idInput.value);
+    // console.log(card);
+    card.style.color = colorInput.value;
+}
+
+// console.log(idInput);
+// console.log(colorInput);
